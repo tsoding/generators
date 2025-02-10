e2424ed0220e6e81fa315761dc6bb1deb2e7b1b1
DarkCat5501 <vicente256@gmail.com>
2025-02-10 12:50:05 -0300

    WIP: Adjusted example

--- examples/fib.c
+++ examples/fib.c
@@ -20,13 +20,4 @@ int main()
     foreach (value, g, (void*)(1000*1000)) {
         printf("%ld\n", (long)value);
     }
-    printf("Final G: %p, %p\n", g->rsp, g->stack_base);
-    /* generator_destroy(g); */
-    
-    /* Generator *g2 = generator_create(fib); */
-    /* foreach (value, g2, (void*)(1000*1000)) { */
-    /*     printf("%ld\n", (long)value); */
-    /* } */
-    /* generator_destroy(g2); */
-
 }
